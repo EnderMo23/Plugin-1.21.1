9374dacc04543731dc1a6848926f638317dac1a9
SirMo_ <fischer.moritz09@gmail.com>
2024-11-15 17:59:34 +0100

    First Commit

---
+++ src/main/kotlin/me/sirmo/serverPlugin/ServerPlugin.kt
@@ -0,0 +1,14 @@
+package me.sirmo.serverPlugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ServerPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
